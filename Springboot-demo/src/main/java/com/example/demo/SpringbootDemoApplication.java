c6d154bc142e61f7d60eaae4a91d23531af065b2
Notoss <stathis.notos@gmail.com>
2020-02-21 20:25:05 +0200

    testing commit

--- Springboot-demo/src/main/java/com/example/demo/SpringbootDemoApplication.java
+++ Springboot-demo/src/main/java/com/example/demo/SpringbootDemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringbootDemoApplication {
 
     public static void main(String[] args) {
+        //test
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
 
